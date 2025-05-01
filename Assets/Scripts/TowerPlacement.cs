ab0fad9a312218b1d29d84eff0239d68f540c26f
Juke422B <jherron@mail.bradley.edu>
2025-05-01 14:07:22 -0500

    save

--- Assets/Scripts/TowerPlacement.cs
+++ Assets/Scripts/TowerPlacement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -13,6 +14,8 @@ public class TowerPlacement : MonoBehaviour
     private bool colorChanged = false;
     [SerializeField] private Sprite normalSprite;
     [SerializeField] private Sprite clickedSprite;
+    [SerializeField] private GameObject descriptionBackground;
+    [SerializeField] private TMP_Text descriptionText;
 
     private void Start()
     {
@@ -26,6 +29,7 @@ public class TowerPlacement : MonoBehaviour
         {
             colorChanged = false;
             GetComponent<SpriteRenderer>().sprite = normalSprite;
+            descriptionBackground.SetActive(false);
         }
     }
 
@@ -42,6 +46,32 @@ public class TowerPlacement : MonoBehaviour
         foreach (GameObject GreenTiles in greenTiles)
         {
             GreenTiles.GetComponent<GreenTileScript>().selected = false;
+        }
+
+        if (TowerType == 1)
+        {
+            descriptionBackground.SetActive(true);
+            descriptionText.text = "Wolfsbane Tower: put desc. here";
+        }
+        else if (TowerType == 2)
+        {
+            descriptionBackground.SetActive(true);
+            descriptionText.text = "Dandelion Tower: put desc. here";
+        }
+        else if (TowerType == 3)
+        {
+            descriptionBackground.SetActive(true);
+            descriptionText.text = "Spiderlily Tower: put desc. here";
+        }
+        else if (TowerType == 4)
+        {
+            descriptionBackground.SetActive(true);
+            descriptionText.text = "Dragonfruit Tower: put desc. here";
+        }
+        else if (TowerType == 5)
+        {
+            descriptionBackground.SetActive(true);
+            descriptionText.text = "Upgrade Towers: put desc. here";
         }
     }
 }
